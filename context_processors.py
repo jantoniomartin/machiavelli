5a8aca6d910f68b833348767d55b1a20916d14d8
Jose Antonio Martin Prieto <jantonio.martin@gmail.com>
2011-11-23 09:34:12 +0100

    fixed bug in context_processors.py

--- context_processors.py
+++ context_processors.py
@@ -2,12 +2,15 @@ from django.conf import settings
 
 def banners(request):
 	context = {}
-	conf = settings.BANNERS
-	
-	for k, v in conf.items():
-		if not v['banner'] is None and v['banner'] != "":
-			context.update({'%s_banner' % k: v['banner']})
-		if not v['url'] is None:
-			context.update({'%s_url' % k: v['url']})
+	try:
+		conf = settings.BANNERS
+	except AttributeError:
+		return context
+	else:
+		for k, v in conf.items():
+			if not v['banner'] is None and v['banner'] != "":
+				context.update({'%s_banner' % k: v['banner']})
+			if not v['url'] is None:
+				context.update({'%s_url' % k: v['url']})
 	return context
 			
