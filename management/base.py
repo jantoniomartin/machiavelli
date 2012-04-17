1a27a2ac0baa31b5389466fac881d4790020b5e4
Jose Antonio Martin Prieto <jantonio.martin@gmail.com>
2012-04-17 12:47:23 +0200

    add new_revolution notice

--- management/base.py
+++ management/base.py
@@ -19,6 +19,9 @@ if "notification" in settings.INSTALLED_APPS:
 		notification.create_notice_type("received_ducats",
 										_("Ducats received"),
 										_("you have received some ducats"))
+		notification.create_notice_type("new_revolution",
+										_("New revolution"),
+										_("you skipped your turn"))
 		notification.create_notice_type("overthrow_attempt",
 										_("Overthrow attempt"),
 										_("you are being overthrown"))
