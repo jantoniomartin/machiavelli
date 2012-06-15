4ab83a9714f58dc8bd0c59032c13cd9b842e020d
Jose Antonio Martin Prieto <jantonio.martin@gmail.com>
2012-06-15 11:40:22 +0200

    add activity and latest_gossip context processors

--- context_processors.py
+++ context_processors.py
@@ -1,4 +1,7 @@
 from django.conf import settings
+from django.core.cache import cache
+
+import machiavelli.models as machiavelli
 
 def banners(request):
 	context = {}
@@ -13,4 +16,23 @@ def banners(request):
 			if not v['url'] is None:
 				context.update({'%s_url' % k: v['url']})
 	return context
-			
+
+def activity(request):
+	cache_key = "activity"
+	context = cache.get(cache_key)
+	if not context:
+		context = {}
+		context['activity'] = machiavelli.Player.objects.exclude(user__isnull=True).values("user").distinct().count()
+		context['games'] = machiavelli.LiveGame.objects.count()
+		cache.set(cache_key, context)
+	return context
+
+def latest_gossip(request):
+	cache_key = "latest_gossip"
+	context = cache.get(cache_key)
+	if not context:
+		context = {}
+		context['whispers'] = machiavelli.Whisper.objects.all()[:5]
+		cache.set(cache_key, context)
+	return context
+
