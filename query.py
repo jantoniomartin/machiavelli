356d8835af0aa956d7fd849ce44772fcff0139a2
Jose Antonio Martin Prieto <jantonio.martin@gmail.com>
2013-12-03 13:18:05 +0100

    add new queries to GameQuerySet

--- query.py
+++ query.py
@@ -13,6 +13,18 @@ class GameQuerySet(models.query.QuerySet):
 		if user:
 			qs = qs.filter(player__user=user)
 		return qs
+	
+	def in_progress(self, user=None):
+		qs = self.filter(started__isnull=False, finished__isnull=True)
+		if user:
+			qs = qs.filter(player__user=user)
+		return qs
 
 	def finished(self):
 		return self.filter(finished__isnull=False)
+
+	def private(self):
+		return self.filter(private=True)
+
+	def by_teams(self):
+		return self.filter(teams__gt=0) 
